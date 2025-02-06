59854fa55ebe7e6f67aac912466a6c2852ed4bf7
“Yagiz <28293527s@student.gla.ac.uk>
2025-02-06 19:48:59 +0000

    a

--- rango/urls.py
+++ rango/urls.py
@@ -4,6 +4,6 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('about/', views.about, name = 'about')
+    path('', views.index, name='index'),  # Maps to /rango/
+    path('about/', views.about, name='about'),  # Maps to /rango/about/
 ]
