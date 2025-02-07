6137b476ced08abb6f0f405c3b36d83736776a03
“Yagiz <28293527s@student.gla.ac.uk>
2025-02-07 22:50:39 +0000

    d

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)  # Added
+    likes = models.IntegerField(default=0)  # Added
+
 
     class Meta:
         verbose_name_plural = 'Categories'
