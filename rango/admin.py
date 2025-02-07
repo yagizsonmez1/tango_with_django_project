654636d4ba588afe45423fa27b1910bc530acf0b
“Yagiz <28293527s@student.gla.ac.uk>
2025-02-07 23:01:48 +0000

    d

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'url')  # Order matches your screenshot
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
