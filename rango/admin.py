de95db62010c39c2bd8f0711aea2d35b01174e3d
“Yagiz <28293527s@student.gla.ac.uk>
2025-02-07 23:06:43 +0000

    d

--- rango/admin.py
+++ rango/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 class PageAdmin(admin.ModelAdmin):
-    list_display = ('category', 'title', 'url')  # Order matches your screenshot
+    # Changed order: Title first, then Category, then URL
+    list_display = ('title', 'category', 'url')  # 🔄 Order swapped
 
 admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
