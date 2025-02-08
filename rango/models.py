46e52d559be4cda6b31316d71f88d714f2bf2f75
“Yagiz <28293527s@student.gla.ac.uk>
2025-02-08 12:10:16 +0000

    d

--- rango/models.py
+++ rango/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-    views = models.IntegerField(default=0)  # Added
-    likes = models.IntegerField(default=0)  # Added
+    name = models.CharField(max_length=128, unique=True) 
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name) 
+        super(Category, self).save(*args, **kwargs)
 
 
     class Meta:
