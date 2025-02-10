9289fc2304ab096421676e11a99383d77eaef1e5
“Yagiz <28293527s@student.gla.ac.uk>
2025-02-10 13:44:47 +0000

    chapter9 hopefully finished

--- rango/forms.py
+++ rango/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from rango.models import Page, Category
 
 from django.contrib.auth.models import User 
+
 from rango.models import UserProfile
 
 class CategoryForm(forms.ModelForm):
