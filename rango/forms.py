9e40c333b86ff54113642881b9bfd59fba54c07d
“Yagiz <28293527s@student.gla.ac.uk>
2025-02-10 13:50:05 +0000

    chapter9 small fix

--- rango/forms.py
+++ rango/forms.py
@@ -50,7 +50,7 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username', 'email', 'password',)
 
-    class UserProfileForm(forms.ModelForm): 
-        class Meta:
-            model = UserProfile
-            fields = ('website', 'picture',)
+class UserProfileForm(forms.ModelForm): 
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
