636253e60f29ff0b289dd96651316f7450433ca7
“Yagiz <28293527s@student.gla.ac.uk>
2025-02-10 11:32:06 +0000

    chapter8 update views

--- rango/views.py
+++ rango/views.py
@@ -21,7 +21,7 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+    return render(request, 'rango/about.html',{})
 
 def show_category(request, category_name_slug):
     # Create a context dictionary to pass to the template
