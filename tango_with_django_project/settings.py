e2d911fddc1f9654265fd142cfa4e982133f5794
“Yagiz <28293527s@student.gla.ac.uk>
2025-02-10 13:27:08 +0000

    chapter8 update settings

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -109,6 +109,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
