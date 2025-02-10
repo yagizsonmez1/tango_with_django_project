af7bdbdc9ea5561e22d4ef80e58ed83bfd975098
“Yagiz <28293527s@student.gla.ac.uk>
2025-02-10 23:00:05 +0000

    chapter9 test fix

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,24 +16,21 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
-LOGIN_URL = 'rango:login'
-
-
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd&=z*s1fs%g=z(jbyp-krb1d_ax9ob#qjs5fuzpygr6f74)u_l'
+SECRET_KEY = 'j!05hkuv_xx^04$%1rgmt#mh5_&*lgc0s7)ac8)0(=$s2byqm^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -46,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    
 ]
 
 MIDDLEWARE = [
@@ -67,11 +65,12 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
+                'django.template.context_processors.debug', 
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
+                'django.contrib.auth.context_processors.auth', 
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media', # Check/add this line!
+                'django.template.context_processors.media', 
+    
             ],
         },
     },
@@ -135,3 +134,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+LOGIN_URL = 'rango:login'
